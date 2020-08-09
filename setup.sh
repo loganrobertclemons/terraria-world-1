5cd37afdcc6b3bc96ae3460cb801a9aa1889a96b
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-09 18:18:18 -0500

    .

--- setup.sh
+++ setup.sh
@@ -1,25 +1,26 @@
 #ssh-keygen -t rsa -N '' -f ./flux/id_rsa -C flux <<< y
 #echo "add the following to your github repo "
 #cat ./flux/id_rsa.pub
-export REGION=us-central1
+export REGION=us-west1
 export ZONE=$REGION-b
 export NAME=terraria-world-1
 export WORLD=worlds_World_1.wld
 export A=$(echo $(gcloud iam service-accounts list   --filter='email ~ [0-9]*-compute@.*'   --format='(email)'|grep compute))
+echo "compute service account is" $A
 cat <<EOF >>startup-script.sh
 #! /bin/bash
 sudo mkdir /tmp/world
 gsutil cp https://storage.cloud.google.com/$(echo $DEVSHELL_PROJECT_ID)-$(echo $NAME)/$(echo $WORLD)?authuser=1 /tmp/world/world/$(echo $WORLD)
 EOF
-gsutil mb gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
+gsutil mb -b gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
 gsutil cp ./$WORLD gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
-gsutil mb gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+gsutil mb -b gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
+gsutil defacl ch -u $A:READ gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/
+gsutil defacl ch -u $A:READ  gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/
 gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-startup-scripts/startup-script.sh,disable-legacy-endpoints=true
-echo "compute service account is" $A
-gsutil acl ch -d $A:READ gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
-gsutil acl ch -d $A:READ gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
+
 kubectl create namespace flux
 kubectl create secret generic flux-ssh --from-file=identity=./flux/id_rsa -n flux
 cd flux
--- startup-script.sh
+++ startup-script.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 sudo mkdir /tmp/world
 gsutil cp https://storage.cloud.google.com/exam-prep-285301-terraria-world-1/worlds_World_1.wld?authuser=1 /tmp/world/world/worlds_World_1.wld
+#! /bin/bash
+sudo mkdir /tmp/world
+gsutil cp https://storage.cloud.google.com/exam-prep-285301-terraria-world-1/worlds_World_1.wld?authuser=1 /tmp/world/world/worlds_World_1.wld
