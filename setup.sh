d45497accd5895e3eca17a0c59bc45fb2ff3b131
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-09 11:28:41 -0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+git clone https://github.com/ammilam/terraria-world-1.git 
 kubectl create namespace flux
-kubectl create secret generic flux-ssh --from-file=identity=./flux/id_rsa -n flux
-./flux/installFlux.sh 
-./flux/installHelmOperator.sh
+kubectl create secret generic flux-ssh --from-file=identity=./terraria-world-1/flux/id_rsa -n flux
+cd ./terraria-world-1/flux
+./installFlux.sh andrew
+./installHelmOperator.sh
 gsutil mb gs://terraria-world-1
-gsutil cp worlds_World_1.wld gs://terraria-world-1/worlds_World_1.wld
+gsutil cp ../worlds_World_1.wld gs://terraria-world-1/worlds_World_1.wld
