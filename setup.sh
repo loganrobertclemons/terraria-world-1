9d11cdbb4796fa4c90be29e136819a4371c15c8f
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-09 16:11:34 -0500

    .

--- setup.sh
+++ setup.sh
@@ -2,16 +2,13 @@ export NAME=terraria-world-1
 export WORLD=worlds_World_1.wld
 cat <<EOF >>startup-script.sh
 #! /bin/bash
-sudo apt-get update -y
-curl https://sdk.cloud.google.com | bash
-exec -l $SHELL
-mkdir /tmp/worlds
-gsutil cp gs://$(echo $DEVSHELL_PROJECT_ID)-startup-script/startup-script.sh /tmp/worlds
+sudo mkdir /tmp/world
+gsutil cp gs://$(echo $DEVSHELL_PROJECT_ID)-startup-script/startup-script.sh /tmp/world
 EOF
 gsutil mb gs://$DEVSHELL_PROJECT_ID-startup-script
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh
 gsutil acl ch -d $A:READ gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh
-gcloud container clusters create $NAME-$DEVSHELL_PROJECT_ID --zone us-central1-a --metadata=startup-script-url=gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh
+gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.15.12-gke.2" --machine-type "e2-medium" --image-type "UBUNTU" --disk-type "pd-standard" --disk-size "100" --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh,disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/$DEVSHELL_PROJECT_ID/global/networks/default" --subnetwork "projects/$DEVSHELL_PROJECT_ID/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
 export A=$(gcloud iam service-accounts list   --filter='email ~ [0-9]*-compute@.*'   --format='(email)'|grep compute)
 kubectl create namespace flux
 kubectl create secret generic flux-ssh --from-file=identity=./flux/id_rsa -n flux
--- startup-script.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo apt-get update -y
-curl https://sdk.cloud.google.com | bash
-exec -l /bin/bash
-mkdir /tmp/worlds
-gsutil cp gs://exam-prep-285301-startup-script/startup-script.sh /tmp/worlds
