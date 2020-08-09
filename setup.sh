ec07b0ddfc23de685eca6212ef4a6c14cebadbdf
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-09 18:05:18 -0500

    .

--- setup.sh
+++ setup.sh
@@ -1,19 +1,28 @@
+#ssh-keygen -t rsa -N '' -f ./flux/id_rsa -C flux <<< y
+#echo "add the following to your github repo "
+#cat ./flux/id_rsa.pub
+export REGION=us-central1
+export ZONE=$REGION-b
 export NAME=terraria-world-1
 export WORLD=worlds_World_1.wld
+export A=$(echo $(gcloud iam service-accounts list   --filter='email ~ [0-9]*-compute@.*'   --format='(email)'|grep compute))
 cat <<EOF >>startup-script.sh
 #! /bin/bash
 sudo mkdir /tmp/world
-gsutil cp gs://$(echo $DEVSHELL_PROJECT_ID)-startup-script/startup-script.sh /tmp/world
+gsutil cp https://storage.cloud.google.com/$(echo $DEVSHELL_PROJECT_ID)-$(echo $NAME)/$(echo $WORLD)?authuser=1 /tmp/world/world/$(echo $WORLD)
 EOF
-gsutil mb gs://$DEVSHELL_PROJECT_ID-startup-script
-gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh
-gsutil acl ch -d $A:READ gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh
-gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.15.12-gke.2" --machine-type "e2-medium" --image-type "UBUNTU" --disk-type "pd-standard" --disk-size "100" --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-startup-script/startup-script.sh,disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/$DEVSHELL_PROJECT_ID/global/networks/default" --subnetwork "projects/$DEVSHELL_PROJECT_ID/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
-export A=$(gcloud iam service-accounts list   --filter='email ~ [0-9]*-compute@.*'   --format='(email)'|grep compute)
+gsutil mb gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
+gsutil cp ./$WORLD gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
+gsutil mb gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
+gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
+gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-startup-scripts/startup-script.sh,disable-legacy-endpoints=true
+echo "compute service account is" $A
+gsutil acl ch -d $A:READ gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
+gsutil acl ch -d $A:READ gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
 kubectl create namespace flux
 kubectl create secret generic flux-ssh --from-file=identity=./flux/id_rsa -n flux
-./flux/installFlux.sh andrew
-./flux/installHelmOperator.sh
-gsutil mb gs://$DEVSHELL_PROJECT_ID-terraria-world-1
-gsutil cp ./$WORLD gs://$DEVSHELL_PROJECT_ID-terraria-world-1/$WORLD
-gsutil acl ch -d $A:READ gs://$NAME/$WORLD
+cd flux
+./installFlux.sh andrew
+./installHelmOperator.sh
+rm startup-script.sh
