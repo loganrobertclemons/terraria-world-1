517bd666ec1ee8f474e8801a07728540db1c475d
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-09 19:04:01 -0500

    .

--- setup.sh
+++ setup.sh
@@ -5,25 +5,32 @@ export REGION=us-west1
 export ZONE=$REGION-b
 export NAME=terraria-world-1
 export WORLD=worlds_World_1.wld
-export A=$(echo $(gcloud iam service-accounts list   --filter='email ~ [0-9]*-compute@.*'   --format='(email)'|grep compute))
-echo "compute service account is" $A
+
 cat <<EOF >>startup-script.sh
 #! /bin/bash
 sudo mkdir /tmp/world
-gsutil cp https://storage.cloud.google.com/$(echo $DEVSHELL_PROJECT_ID)-$(echo $NAME)/$(echo $WORLD)?authuser=1 /tmp/world/world/$(echo $WORLD)
+gsutil cp gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD /tmp/world/world/$(echo $WORLD)
 EOF
-gsutil mb -b gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
+
+gsutil mb -b off gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
 gsutil cp ./$WORLD gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
-gsutil mb -b gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+gsutil mb -b off gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+
+export COMPUTE_ENGINE_SA_EMAIL=$(gcloud iam service-accounts list --filter="name:Compute Engine default service account" --format "value(email)")
+gsutil iam ch serviceAccount:${COMPUTE_ENGINE_SA_EMAIL}:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+gsutil iam ch serviceAccount:${COMPUTE_ENGINE_SA_EMAIL}:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
+
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
-gsutil defacl ch -u $A:READ gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/
-gsutil defacl ch -u $A:READ  gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/
-gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-startup-scripts/startup-script.sh,disable-legacy-endpoints=true
+
+gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh,disable-legacy-endpoints=true
 
 kubectl create namespace flux
 kubectl create secret generic flux-ssh --from-file=identity=./flux/id_rsa -n flux
 cd flux
+echo "place this in git as a deploy key called flux-ssh"
+cat id_rsa.pub
 ./installFlux.sh andrew
 ./installHelmOperator.sh
+
 rm startup-script.sh
