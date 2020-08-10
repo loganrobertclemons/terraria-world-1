4dfa8ba2d61ab291427926dd55de91b35aa62b69
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-09 23:03:01 -0500

    .

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #sets environment variables
-export REGION=us-east1
+export REGION=us-central1
 export ZONE=$REGION-b
 export NAME=terraria-world-1
 export WORLD=worlds_World_1.wld
@@ -15,27 +15,33 @@ read -sp 'GitHub Password: ' PASSWORD
 #creates startup script that the k8s vms will use
 cat <<EOF >>startup-script.sh
 #! /bin/bash
-sudo mkdir /tmp/world
 #this copies down the world file from the project specific world storage bucket
-gsutil cp gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD /tmp/world/world/$(echo $WORLD)
+sudo apt-get update -y
+sudp apt install wget
+sudo mkdir /tmp/world
+touch test.txt
+wget https://storage.cloud.google.com/$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD /tmp/world/world/$WORLD
 EOF
 
 #creates the world file storage bucket, copies the file to it from this repo, and assigns permissions to ce service account
 gsutil mb -b on gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
 gsutil cp ./$WORLD gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
-gsutil iam ch serviceAccount:$COMPUTE_ENGINE_SA_EMAIL:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
+#gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
 
 #creates the startup scripts storage bucket, copies the file created above, and sets permissions to ce service account
 gsutil mb -b on gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
-gsutil iam ch serviceAccount:$COMPUTE_ENGINE_SA_EMAIL:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+#gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
 
-gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE --metadata startup-script-url=gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh,disable-legacy-endpoints=true
+#creates the k8s cluster in the sepcified region/zone with the generated startup script
+gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE  --image-type "UBUNTU" --scopes "https://www.googleapis.com/auth/cloud-platform" --metadata startup-script='#! /bin/bash \sudo apt-get update -y \ sudo apt install wget \ sudo mkdir /tmp/world wget https://storage.cloud.google.com/$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD /tmp/world EOF',disable-legacy-endpoints=true
 
+#creates the flux namespace and generates an ssh key
 kubectl create namespace flux
 ssh-keygen -t rsa -N '' -f ./flux/id_rsa -C flux <<< y
 kubectl create secret generic flux-ssh --from-file=identity=./flux/id_rsa -n flux
 
+#creates an executable to be invoked that creates a git deploy key on the repo specified above
 cat <<EOF >>git-key-deploy.sh
 #! /bin/bash
 curl \
@@ -45,9 +51,9 @@ curl \
   https://api.github.com/repos/$USERNAME/$REPO/keys \
   -d '{"key":"$(cat ./flux/id_rsa.pub)","title":"flux-ssh"}'
 EOF
-
 sudo chmod +x git-key-deploy.sh
 ./git-key-deploy.sh
+rm git-key-deploy.sh
 
 cd flux
 ./installFlux.sh andrew
--- startup-script.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo mkdir /tmp/world
-gsutil cp gs://exam-prep-285301-us-west1-terraria-world-1/worlds_World_1.wld /tmp/world/world/worlds_World_1.wld
-#! /bin/bash
-sudo mkdir /tmp/world
-#this copies down the world file from the project specific world storage bucket
-gsutil cp gs://exam-prep-285301-us-east1-terraria-world-1/worlds_World_1.wld /tmp/world/world/worlds_World_1.wld
