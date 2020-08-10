95c0d07bd8ff1c09185e5f7dd6fbbab6b8278729
Andrew Milam <andrewmichaelmilam@gmail.com>
2020-08-10 09:10:49 -0500

    .

--- setup.sh
+++ setup.sh
@@ -26,15 +26,15 @@ EOF
 #creates the world file storage bucket, copies the file to it from this repo, and assigns permissions to ce service account
 gsutil mb -b on gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
 gsutil cp ./$WORLD gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD
-#gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
+gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-$NAME
 
 #creates the startup scripts storage bucket, copies the file created above, and sets permissions to ce service account
 gsutil mb -b on gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
-#gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
+gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts
 gsutil cp ./startup-script.sh gs://$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh
 
 #creates the k8s cluster in the sepcified region/zone with the generated startup script
-gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE  --image-type "UBUNTU" --scopes "https://www.googleapis.com/auth/cloud-platform" --metadata startup-script='#! /bin/bash \sudo apt-get update -y \ sudo apt install wget \ sudo mkdir /tmp/world wget https://storage.cloud.google.com/$DEVSHELL_PROJECT_ID-$REGION-$NAME/$WORLD /tmp/world EOF',disable-legacy-endpoints=true
+gcloud beta container --project $DEVSHELL_PROJECT_ID clusters create $NAME --zone $ZONE  --image-type "UBUNTU" --scopes "https://www.googleapis.com/auth/cloud-platform" --metadata startup-script-url=https://storage.cloud.google.com/$DEVSHELL_PROJECT_ID-$REGION-startup-scripts/startup-script.sh,disable-legacy-endpoints=true
 
 #creates the flux namespace and generates an ssh key
 kubectl create namespace flux
@@ -55,6 +55,7 @@ sudo chmod +x git-key-deploy.sh
 ./git-key-deploy.sh
 rm git-key-deploy.sh
 
+#installs flux and helm operator that will kick off and manage the terraria server deployment
 cd flux
 ./installFlux.sh andrew
 ./installHelmOperator.sh
